a4b168f7dbfe5143011c5248b98c0faaf305ecf2
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-03-09 17:55:29 +0100

    updated function

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,34 +5,32 @@
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
-    """
-
-    if word_dict is None:
-        word_dict = {}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after} if after else {}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if response.status_code != 200:
-        return
-    data = response.json()
-    for post in data["data"]["children"]:
-        title = post["data"]["title"].lower()
-        for word in word_list:
-            if (f" {word.lower()} " in f" {title} " 
-                or title.startswith(f"{word.lower()} ") 
-                or title.endswith(f" {word.lower()}")
-                or title == f"{word.lower()}"):
-                word_dict[word.lower()] = word_dict.get(word.lower(), 0) + 1
-    if data["data"]["after"] is not None:
-        word_dict = count_words(subreddit, word_list, data["data"]["after"], word_dict)
-    else:
-        sorted_counts = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+def count_words(subreddit, word_list, after=None, counts={}):
+    # Base case: no more articles to fetch
+    if after == "":
+        # Sort the counts by count (descending) and then by word (ascending)
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
         for word, count in sorted_counts:
             print(f"{word}: {count}")
+        return
+    
+    # Query the Reddit API for hot articles in the subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"after": after}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    # Base case: invalid subreddit or no more articles to fetch
+    if response.status_code != 200:
+        return
+    
+    # Recursively call count_words with the next "after" parameter
+    data = response.json()
+    children = data["data"]["children"]
+    for child in children:
+        title = child["data"]["title"].lower()
+        for word in word_list:
+            # Ignore words with special characters (e.g. java. or java!)
+            if f"{word.lower()} " in title:
+                counts[word.lower()] = counts.get(word.lower(), 0) + title.count(f"{word.lower()} ")
+    count_words(subreddit, word_list, data["data"]["after"], counts)
